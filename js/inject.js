228d1485a5ee5528f288cd0cc8bd1bb0ee699c50
Max <mail@maxi-muth.de>
2014-03-30 22:21:31 +0200

    Added test-case in inject.js, mods in manifest

---
+++ js/inject.js
@@ -0,0 +1,10 @@
+/* 
+Process Flow:
+1. Pass the Google Search Query to Wolfram|Alpha and get the result as XML
+1.1. http://api.wolframalpha.com/v2/query?input=sunrise+munich&appid=8X6XE5-Q5887TY7TE (only 2000 Requests / Month with that App ID...)
+2. Parse that XML to get a HTML-Object of the Result
+3. Inject this object beatifully into the Google Site
+*/
+
+// Just for testing the url Pattern in the manifest
+$("resultStats").update("Oha, it works!");
